32543f42cad462478ec1f5721c9fd12508c7a6d0
Izayakaban <116605856+Izayakaban@users.noreply.github.com>
2026-01-21 12:25:40 -0600

    First Upload

---
+++ CeaserQuestions.js
@@ -0,0 +1,21 @@
+
+//Question 1:-------------------------------------------------------------------------------------------------
+//meetingSpot
+//shift
+//revealedMessage
+//disguisedMessage
+
+//Question 2:-------------------------------------------------------------------------------------------------
+// meetingSpot; STRING
+// shift; NUMBER
+// revealedMessage; STRING
+// disguisedMessage; STRING
+
+//Question 3:-------------------------------------------------------------------------------------------------
+let meetingSpot = "garden";
+const shift = 2;
+let revealedMessage = ""; //will hold input or provide output
+let disguisedMessage = ""; //will hold input or provide output
+
+//Question 4:-------------------------------------------------------------------------------------------------
+Number.isInteger(shift); //true
